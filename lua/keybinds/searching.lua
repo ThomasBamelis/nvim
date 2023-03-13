b0dd6086dea13f545ef3bb1c157c4a776120bb7b
Thomas Bamelis <thomas@bamelis.dev>
2023-03-13 22:18:22 +0100

    change nvim_keymap_set to vim.keymap.set

--- lua/keybinds/searching.lua
+++ lua/keybinds/searching.lua
@@ -2,32 +2,32 @@
 local opts = {noremap = true, silent = true}
 
 opts.desc = 'next search match'
-vim.api.nvim_set_keymap('n', 'n',
+vim.keymap.set('n', 'n',
     [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
     opts)
 opts.desc = 'prev search match'
-vim.api.nvim_set_keymap('n', 'N',
+vim.keymap.set('n', 'N',
     [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
     opts)
 -- TODO find out what this do, is normal vim remap to hlslens
 opts.desc = '* search'
-vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], opts)
+vim.keymap.set('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], opts)
 opts.desc = '# search'
-vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], opts)
+vim.keymap.set('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], opts)
 opts.desc = 'g* search'
-vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], opts)
+vim.keymap.set('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], opts)
 opts.desc = 'g# search'
-vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], opts)
+vim.keymap.set('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], opts)
 
 opts.desc = 'noh command'
-vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', opts)
+vim.keymap.set('n', '<Leader>l', '<Cmd>noh<CR>', opts)
 
 -- LeaderF config
 opts.desc = 'grep through repo'
-vim.api.nvim_set_keymap('n', '<Leader>g', '<Cmd>Leaderf rg<CR>', opts)
+vim.keymap.set('n', '<Leader>g', '<Cmd>Leaderf rg<CR>', opts)
 opts.desc = 'Show recently opened files'
-vim.api.nvim_set_keymap('n', '<Leader>r', '<Cmd>Leaderf mru<CR>', opts)
+vim.keymap.set('n', '<Leader>r', '<Cmd>Leaderf mru<CR>', opts)
 opts.desc = 'Search for file'
-vim.api.nvim_set_keymap('n', '<Leader>f', '<Cmd>LeaderfFile<CR>', opts)
+vim.keymap.set('n', '<Leader>f', '<Cmd>LeaderfFile<CR>', opts)
 opts.desc = 'Search in buffer'
-vim.api.nvim_set_keymap('n', '<Leader>b', '<Cmd>LeaderfBuffer<CR>', opts)
+vim.keymap.set('n', '<Leader>b', '<Cmd>LeaderfBuffer<CR>', opts)
