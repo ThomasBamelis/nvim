f92ade997cd814c05ae09a2d08682f8a7afae60f
Thomas Bamelis <thomas@bamelis.dev>
2024-10-29 19:38:45 +0100

    Fix Mason installs, WARNING stylua enforces tabs

--- lua/plugins/coding/language-servers/mason.lua
+++ lua/plugins/coding/language-servers/mason.lua
@@ -1,83 +1,87 @@
 return {
-  -- Installer for language servers
-  {
+	-- Installer for language servers
+	{
 
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    keys = { { "<leader>pm", "<cmd>Mason<cr>", desc = "Mason" } },
-    build = ":MasonUpdate",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "",
-          package_pending = "",
-          package_uninstalled = "",
-        },
-      },
-      -- Install formatters for conform here
-      ensure_installed = {
-        "shfmt",        -- sh
-        "isort",        -- python
-        "black",        -- python
-        "clang-format", -- C/C++
-        "rustfmt",      -- Rust
-        "amsfmt",       -- Assembly
-        "cmakelang",    -- CMake
-        "deno",         -- Javascript, JSON, Markdown
-        "stylua",       -- lua
-        "codespell",    -- non intrusive spell checking
-      },
-    },
-    ---@param opts MasonSettings | {ensure_installed: string[]}
-    --config = function(_, opts)
-    --	require("mason").setup(opts)
-    --	local mr = require("mason-registry")
-    --	mr:on("package:install:success", function()
-    --		vim.defer_fn(function()
-    --			-- trigger FileType event to possibly load this newly installed LSP server
-    --			require("lazy.core.handler.event").trigger({
-    --				event = "FileType",
-    --				buf = vim.api.nvim_get_current_buf(),
-    --			})
-    --		end, 100)
-    --	end)
-    --
-    --	mr.refresh(function()
-    --		for _, tool in ipairs(opts.ensure_installed) do
-    --			local p = mr.get_package(tool)
-    --			if not p:is_installed() then
-    --				p:install()
-    --			end
-    --		end
-    --	end)
-    --end,
-  },
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		keys = { { "<leader>pm", "<cmd>Mason<cr>", desc = "Mason" } },
+		build = ":MasonUpdate",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "",
+					package_pending = "",
+					package_uninstalled = "",
+				},
+			},
+			-- Install formatters for conform here
+			ensure_installed = {
+				"shfmt", -- sh
+				"isort", -- python
+				"black", -- python
+				"clang-format", -- C/C++
+				--"rustfmt", -- Rust: must be installed with rustup, not mason
+				"asmfmt", -- Assembly
+				"cmakelang", -- CMake
+				"deno", -- Javascript, JSON, Markdown
+				"stylua", -- lua
+				"codespell", -- non intrusive spell checking
+			},
+		},
+		---@param opts MasonSettings | {ensure_installed: string[]}
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
+			mr:on("package:install:success", function()
+				vim.defer_fn(function()
+					-- trigger FileType event to possibly load this newly installed LSP server
+					require("lazy.core.handler.event").trigger({
+						event = "FileType",
+						buf = vim.api.nvim_get_current_buf(),
+					})
+				end, 100)
+			end)
 
-  -- Links the mason installed language servers to nvim-lspconfig
-  {
-    "williamboman/mason-lspconfig.nvim",
-    --cmd = "Mason",
-    dependencies = { "williamboman/mason.nvim" },
-    opts = {
-      -- Install nvim-lsp language servers here
-      ensure_installed = {
-        "bashls",                          -- bash
-        "pyright",                         -- python
-        "asm_lsp",                         -- assembly
-        "clangd",                          -- C/C++
-        "cmake",                           -- CMake
-        "dockerls",                        -- Dockerfile
-        "docker_compose_language_service", -- Docker compose
-        "jsonls",                          -- json
-        "lua_ls",                          -- Lua
-        "marksman",                        -- marksman
-        "mesonlsp",                        -- Meson
-        "rust_analyzer",                   -- Rust
-        "verible",                         -- SystemVerilog
-      },
-      automatic_installation = true,
-    },
-    --config = function()
-    --end,
-  },
+			mr.refresh(function()
+				for _, tool in ipairs(opts.ensure_installed) do
+					local p = mr.get_package(tool)
+					if not p:is_installed() then
+						p:install()
+					end
+				end
+			end)
+		end,
+	},
+
+	-- Links the mason installed language servers to nvim-lspconfig
+	{
+		"williamboman/mason-lspconfig.nvim",
+		--cmd = "Mason",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			-- Install nvim-lsp language servers here
+			ensure_installed = {
+				-- language servers
+				"bashls", -- bash
+				"pyright", -- python
+				"asm_lsp", -- assembly
+				"clangd", -- C/C++
+				"cmake", -- CMake
+				"dockerls", -- Dockerfile
+				"docker_compose_language_service", -- Docker compose
+				"jsonls", -- json
+				"lua_ls", -- Lua
+				"marksman", -- marksman
+				"mesonlsp", -- Meson
+				"rust_analyzer", -- Rust
+				"verible", -- SystemVerilog
+			},
+			automatic_installation = true,
+		},
+		--config = function()
+		--end,
+	},
 }
--- lua/plugins/coding/language-servers/nvim-lspconfig.lua
+++ lua/plugins/coding/language-servers/nvim-lspconfig.lua
@@ -1,197 +1,196 @@
 local leader_key = "c"
 local M = {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPost", "BufNewFile" },
-  cmd = { "LspInfo", "LspInstall", "LspUninstall" },
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "nvim-telescope/telescope.nvim",
-    "hrsh7th/nvim-cmp",
-  },
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPost", "BufNewFile" },
+	cmd = { "LspInfo", "LspInstall", "LspUninstall" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"nvim-telescope/telescope.nvim",
+		"hrsh7th/nvim-cmp",
+	},
 }
 
 M.config = function()
-  -- import lspconfig plugin
-  local lspconfig = require("lspconfig")
+	-- import lspconfig plugin
+	local lspconfig = require("lspconfig")
 
-  -- import mason_lspconfig plugin
-  local mason_lspconfig = require("mason-lspconfig")
+	-- import mason_lspconfig plugin
+	local mason_lspconfig = require("mason-lspconfig")
 
-  -- import cmp-nvim-lsp plugin
-  local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	-- import cmp-nvim-lsp plugin
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-  -- Tell nvim-cmp about the capabilities of the language server
-  local capabilities = cmp_nvim_lsp.default_capabilities()
+	-- Tell nvim-cmp about the capabilities of the language server
+	local capabilities = cmp_nvim_lsp.default_capabilities()
 
-  -- Setup the keybinds
-  local keymap = vim.keymap
-  -- Function to execute when a language server will attach.
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-      -- Buffer local mappings.
-      -- See `:help vim.lsp.*` for documentation on any of the below functions
-      local opts = { buffer = ev.buf, silent = true }
+	-- Setup the keybinds
+	local keymap = vim.keymap
+	-- Function to execute when a language server will attach.
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+		callback = function(ev)
+			-- Buffer local mappings.
+			-- See `:help vim.lsp.*` for documentation on any of the below functions
+			local opts = { buffer = ev.buf, silent = true }
 
-      -- Taken by diagnostics: o, j, k, e, E, w, W
+			-- Taken by diagnostics: o, j, k, e, E, w, W
 
-      -- set keybinds
-      opts.desc = "Show LSP references"
-      keymap.set("n", "<leader>" .. leader_key .. "R", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+			-- set keybinds
+			opts.desc = "Show LSP references"
+			keymap.set("n", "<leader>" .. leader_key .. "R", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-      opts.desc = "Go to declaration"
-      keymap.set("n", "<leader>" .. leader_key .. "D", vim.lsp.buf.declaration, opts) -- go to declaration
+			opts.desc = "Go to declaration"
+			keymap.set("n", "<leader>" .. leader_key .. "D", vim.lsp.buf.declaration, opts) -- go to declaration
 
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "<leader>" .. leader_key .. "d", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+			opts.desc = "Show LSP definitions"
+			keymap.set("n", "<leader>" .. leader_key .. "d", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-      opts.desc = "Show LSP implementations"
-      keymap.set("n", "<leader>" .. leader_key .. "i", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+			opts.desc = "Show LSP implementations"
+			keymap.set("n", "<leader>" .. leader_key .. "i", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-      opts.desc = "Show LSP type definitions"
-      keymap.set("n", "<leader>" .. leader_key .. "t", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+			opts.desc = "Show LSP type definitions"
+			keymap.set("n", "<leader>" .. leader_key .. "t", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>" .. leader_key .. "a", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+			opts.desc = "See available code actions"
+			keymap.set({ "n", "v" }, "<leader>" .. leader_key .. "a", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-      opts.desc = "Smart rename"
-      keymap.set("n", "<leader>" .. leader_key .. "r", vim.lsp.buf.rename, opts) -- smart rename
+			opts.desc = "Smart rename"
+			keymap.set("n", "<leader>" .. leader_key .. "r", vim.lsp.buf.rename, opts) -- smart rename
 
-      opts.desc = "Show buffer diagnostics"
-      keymap.set("n", "<leader>" .. leader_key .. "D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+			opts.desc = "Show buffer diagnostics"
+			keymap.set("n", "<leader>" .. leader_key .. "D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-      opts.desc = "Show documentation for what is under cursor (K)"
-      keymap.set("n", "<leader>" .. leader_key .. "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-      keymap.set("n", "K", vim.lsp.buf.hover, opts)                             -- show documentation for what is under cursor
+			opts.desc = "Show documentation for what is under cursor (K)"
+			keymap.set("n", "<leader>" .. leader_key .. "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>" .. leader_key .. "!", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-    end,
-  })
+			opts.desc = "Restart LSP"
+			keymap.set("n", "<leader>" .. leader_key .. "!", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+		end,
+	})
 
-  -- Change the Diagnostic symbols in the sign column (gutter)
-  local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-  end
+	-- Change the Diagnostic symbols in the sign column (gutter)
+	local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
 
-  -- Setup the language servers
-  mason_lspconfig.setup_handlers({
-    -- default handler for installed servers
-    function(server_name)
-      lspconfig[server_name].setup({
-        capabilities = capabilities,
-      })
-    end,
+	-- Setup the language servers
+	mason_lspconfig.setup_handlers({
+		-- default handler for installed servers
+		function(server_name)
+			lspconfig[server_name].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- Bash
-    ["bashls"] = function()
-      lspconfig["bashls"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- Bash
+		["bashls"] = function()
+			lspconfig["bashls"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- Python
-    ["pyright"] = function()
-      lspconfig["pyright"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- Python
+		["pyright"] = function()
+			lspconfig["pyright"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- assembly
-    ["asm_lsp"] = function()
-      lspconfig["asm_lsp"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- assembly
+		["asm_lsp"] = function()
+			lspconfig["asm_lsp"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- C/C++
-    ["clangd"] = function()
-      lspconfig["clangd"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- C/C++
+		["clangd"] = function()
+			lspconfig["clangd"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- CMake
-    ["cmake"] = function()
-      lspconfig["cmake"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- CMake
+		["cmake"] = function()
+			lspconfig["cmake"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- Dockerfile
-    ["dockerls"] = function()
-      lspconfig["dockerls"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- Dockerfile
+		["dockerls"] = function()
+			lspconfig["dockerls"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- Docker compose
-    ["docker_compose_language_service"] = function()
-      lspconfig["docker_compose_language_service"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- Docker compose
+		["docker_compose_language_service"] = function()
+			lspconfig["docker_compose_language_service"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- JSON
-    ["jsonls"] = function()
-      lspconfig["jsonls"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- JSON
+		["jsonls"] = function()
+			lspconfig["jsonls"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- Lua
-    ["lua_ls"] = function()
-      -- configure lua server (with special settings)
-      lspconfig["lua_ls"].setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            -- make the language server recognize "vim" global
-            diagnostics = {
-              globals = { "vim" },
-            },
-            completion = {
-              callSnippet = "Replace",
-            },
-          },
-        },
-      })
-    end,
+		-- Lua
+		["lua_ls"] = function()
+			-- configure lua server (with special settings)
+			lspconfig["lua_ls"].setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						-- make the language server recognize "vim" global
+						diagnostics = {
+							globals = { "vim" },
+						},
+						completion = {
+							callSnippet = "Replace",
+						},
+					},
+				},
+			})
+		end,
 
-    -- Markdown
-    ["marksman"] = function()
-      lspconfig["marksman"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- Markdown
+		["marksman"] = function()
+			lspconfig["marksman"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- Meson
-    ["mesonlsp"] = function()
-      lspconfig["mesonlsp"].setup({
-        capabilities = capabilities,
-      })
-    end,
+		-- Meson
+		["mesonlsp"] = function()
+			lspconfig["mesonlsp"].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-    -- Rust
-    -- TODO: check options here https://github.com/rust-lang/rust-analyzer/blob/master/docs/user/generated_config.adoc
-    ["rust_analyzer"] = function()
-      lspconfig["rust_analyzer"].setup({
-        -- TODO: add experimental.serverStatusNotification = true to capabilities
-        capabilities = capabilities,
-      })
-    end,
+		-- Rust
+		-- TODO: check options here https://github.com/rust-lang/rust-analyzer/blob/master/docs/user/generated_config.adoc
+		["rust_analyzer"] = function()
+			lspconfig["rust_analyzer"].setup({
+				-- TODO: add experimental.serverStatusNotification = true to capabilities
+				capabilities = capabilities,
+			})
+		end,
 
-    -- SystemVerilog
-    ["verible"] = function()
-      lspconfig["verible"].setup({
-        capabilities = capabilities,
-      })
-    end,
-  })
+		-- SystemVerilog
+		["verible"] = function()
+			lspconfig["verible"].setup({
+				capabilities = capabilities,
+			})
+		end,
+	})
 end
 
 return M
