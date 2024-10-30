8fadbb1d80d21c809373fb082c61672075bf4b64
Thomas Bamelis <Thomas.Bamelis@arm.com>
2024-10-30 09:31:41 +0100

    Disable format on save by default, it's messy

--- lua/plugins/coding/editing/conform.lua
+++ lua/plugins/coding/editing/conform.lua
@@ -89,7 +89,8 @@ return {
     init = function()
       -- If you want the formatexpr, here is the place to set it
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-      vim.g.disable_autoformat = false
+      -- Disable autoformatting by default
+      vim.g.disable_autoformat = true
     end,
   },
 }
