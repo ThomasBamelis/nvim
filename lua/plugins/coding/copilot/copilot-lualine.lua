15d223385854a692442e24a460b0b2d07b8004e0
Thomas Bamelis <Thomas.Bamelis@arm.com>
2024-11-27 19:02:46 +0100

    Configure lualine layout and add copilot-lualine

---
+++ lua/plugins/coding/copilot/copilot-lualine.lua
@@ -0,0 +1,10 @@
+return {
+	-- Status line at the bottom
+	{
+    "AndreM222/copilot-lualine",
+    dependencies = {
+      "zbirenbaum/copilot.lua",
+    },
+    lazy = true,
+	},
+}
--- lua/plugins/editor/layout/lualine.lua
+++ lua/plugins/editor/layout/lualine.lua
@@ -2,6 +2,43 @@ return {
 	-- Status line at the bottom
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+		dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      { "AndreM222/copilot-lualine" }, -- for copilot integration
+    },
+    opts = {
+      options = {
+          icons_enabled = true,
+          --theme = cozynight,
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {},
+          always_divide_middle = true
+      },
+      sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff',
+              {
+                  'diagnostics',
+                  sources = { "nvim_diagnostic" },
+                  symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+              }
+          },
+          lualine_c = { 'filename' },
+          lualine_x = { {'copilot', show_colors=true } ,'encoding', 'fileformat', 'filetype' }, -- I added copilot here
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+      },
+      inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {}
+      },
+      tabline = {},
+      extensions = {}
+    },
 	},
 }
